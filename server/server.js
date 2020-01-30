fce410a6c3cf4550df9775a097aaae3d2cb14a1c
Narcisa <minca.narcisa@gmail.com>
2020-01-30 12:07:38 +0200

    init node server

---
+++ server/server.js
@@ -0,0 +1,31 @@
+var express = require("express");
+const bodyParser = require("body-parser");
+var logger = require("morgan");
+
+const app = express();
+
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization "
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(logger("dev"));
+
+app.use((err, req, res, next) => {
+  console.warn(err);
+  res.status(500).send("some error...");
+});
+
+app.listen(3000, function() {
+  console.log("Listening on port 3000!");
+});
