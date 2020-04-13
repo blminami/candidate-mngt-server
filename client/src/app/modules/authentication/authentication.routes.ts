c90502149c607ef1e390a026c40669dbf687767d
Narcisa <minca.narcisa@gmail.com>
2020-04-13 17:50:11 +0300

    remove client from repo

--- client/src/app/modules/authentication/authentication.routes.ts
+++
@@ -1,9 +0,0 @@
-import { LoginComponent } from './components/login/login.component';
-
-export const AUTHENTICATION_ROUTES = [
-    {
-        path: 'login',
-        component: LoginComponent,
-        data: { title: 'Login', id: 'login' }
-    },
-];
