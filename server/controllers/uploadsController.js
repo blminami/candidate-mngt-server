ffa217cbed258bbeb8bcdca94aecccd8d70adc87
Narcisa <minca.narcisa@gmail.com>
2020-05-01 14:21:10 +0300

    uploads

--- server/controllers/uploadsController.js
+++ server/controllers/uploadsController.js
@@ -1,6 +1,22 @@
 import fs from 'fs';
 import { parseFile } from '../helpers/pdf-parser';
 import dbQuery from '../db/dev/dbQuery';
+import { errorMessage, successMessage, status } from '../helpers/status';
+import path from 'path';
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './profiles');
+  },
+  filename: function (req, file, callback) {
+    const { user_id } = req.user;
+    const originalName = file.originalname.split('.')[0];
+    const format = file.originalname.split('.')[1];
+    callback(null, `${originalName}$${user_id}.${format}`);
+  },
+});
+const upload = multer({ storage: storage }).single('fileKey');
 
 const watch = () => {
   fs.watch('profiles', async (eventType, filename) => {
@@ -38,18 +54,55 @@ const watch = () => {
 };
 
 const getAllUploads = async (req, res) => {
+  const { filename } = req.query;
   const { user_id } = req.user;
-  const getAllQuery = `SELECT * FROM uploads WHERE user_id=$1;`;
+  let getAllQuery = `SELECT * FROM uploads WHERE user_id=$1`;
+  if (filename) {
+    getAllQuery += " AND name LIKE '%" + filename + "%';";
+  }
   try {
     const { rows } = await dbQuery.query(getAllQuery, [user_id]);
     const dbResponse = rows;
     successMessage.data = dbResponse;
     return res.status(status.created).send(successMessage);
   } catch (error) {
-    console.log('error', error);
     errorMessage.error = 'Unable to get uploads';
     return res.status(status.error).send(errorMessage);
   }
 };
 
-export { watch, getAllUploads };
+const downloadUpload = async (req, res) => {
+  const { filename } = req.params;
+  const { user_id } = req.user;
+  const name = filename.split('.')[0];
+  const format = filename.split('.')[1];
+  const file = path.resolve(
+    __dirname,
+    `../processed/${name}$${user_id}.${format}`
+  );
+  res.download(file);
+};
+
+const uploadFile = (req, res) => {
+  upload(req, res, async function (err) {
+    if (err) {
+      return res.status(status.error).send('Error uploading file');
+    }
+    let addUpload = `INSERT INTO uploads(id, user_id, type, name, size)
+    VALUES (default, $1, 'document', $2, $3)`;
+    const { user_id } = req.user;
+    const name = req.file.originalname;
+    const size = req.file.size / 1000 + 'kB';
+    try {
+      const { rows } = await dbQuery.query(addUpload, [user_id, name, size]);
+      const dbResponse = rows;
+      successMessage.data = dbResponse;
+      return res.status(status.created).send(successMessage);
+    } catch (error) {
+      errorMessage.error = 'Unable to add uploads';
+      return res.status(status.error).send(errorMessage);
+    }
+  });
+};
+
+export { watch, getAllUploads, downloadUpload, uploadFile };
--- server/routes/uploadsRoute.js
+++ server/routes/uploadsRoute.js
@@ -1,10 +1,16 @@
 import express from 'express';
 
 import verifyAuth from '../middlewares/verifyAuth';
-import { getAllUploads } from '../controllers/uploadsController';
+import {
+  getAllUploads,
+  downloadUpload,
+  uploadFile,
+} from '../controllers/uploadsController';
 
 const router = express.Router();
 
 router.get('/', verifyAuth, getAllUploads);
+router.get('/:filename', verifyAuth, downloadUpload);
+router.post('/', verifyAuth, uploadFile);
 
 export default router;
